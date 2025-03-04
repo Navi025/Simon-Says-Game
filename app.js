b5e1284d00f8aceacf4355332fe7e91428d134c2
Navi025 <vinayaknavnitya20@gmail.com>
2025-03-04 12:39:23 +0530

    -responsiveness

--- app.js
+++ app.js
@@ -1,59 +1,82 @@
-console.log(..."Simon Says Game Console");
-let gameSeq = [];
-let userSeq = [];
-
-let btns = ["yellow", "red", "green", "purple"];
-
-let started = false;
+let gameSequence = [];
+let userSequence = [];
 let level = 0;
+const colors = ["yellow", "red", "green", "purple"];
+const startButton = document.getElementById("start-game");
+const modal = document.getElementById("instruction-modal");
+const levelText = document.getElementById("level");
 
-let btn = document.querySelectorAll(".btn");
-let displayText = document.querySelector("h2");
-
-document.addEventListener("click", function () {
-    if (started == false) {
-        console.log("Game Started");
-        started = true;
-        levelUp();
-    }
+// Hide Modal & Start Game
+startButton.addEventListener("click", function() {
+    modal.style.display = "none"; // Hide instruction popup
+    startGame();
 });
 
-function btnFlash(btn) {
-    btn.classList.add("Flash");
-    console.log("Adding Flash");
-    setTimeout(function () {
-        btn.classList.remove("Flash");
-        console.log("Removing Flash");
-    }, 80);
+// Start Game Function
+function startGame() {
+    level = 1;
+    gameSequence = [];
+    userSequence = [];
+    nextLevel();
+}
 
-};
+// Next Level Function
+function nextLevel() {
+    userSequence = [];
+    levelText.innerText = level;
 
-function userBtnFlash(btn) {
-    btn.classList.add("userFlash");
-    console.log("Adding User Flash");
-    setTimeout(function () {
-        btn.classList.remove("userFlash");
-        console.log("Removing User Flash");
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    gameSequence.push(randomColor);
+
+    flashSequence();
+}
+
+// Flashing Sequence Function
+function flashSequence() {
+    let i = 0;
+    const interval = setInterval(() => {
+        let btn = document.getElementById(gameSequence[i]);
+        btn.classList.add("flash");
+
+        setTimeout(() => {
+            btn.classList.remove("flash");
+        }, 110);
+
+        i++;
+        if (i >= gameSequence.length) {
+            clearInterval(interval);
+        }
     }, 150);
+}
 
-};
+// Button Click Event
+document.querySelectorAll(".btn").forEach(btn => {
+    btn.addEventListener("click", function() {
+        let clickedColor = this.id;
+        userSequence.push(clickedColor);
+        userFlashButton(clickedColor);
+        checkAnswer(userSequence.length - 1);
+    });
+});
 
-function levelUp() {
-    userSeq = [];
-    level++;
-    displayText.innerText = `Level ${level}`;
-    //random btn choose
-    let randIndx = Math.floor(Math.random() * 4);
-    // console.log(randIndx);
-    let randCol = btns[randIndx];
-    // console.log(randCol);
-    let randBtn = document.querySelector(`.${randCol}`)
-    // console.log(randBtn);
-    btnFlash(randBtn);
-    gameSeq.push(randCol);
-    console.log(gameSeq);
+// Flash Clicked Button
+function flashButton(color) {
+    let btn = document.getElementById(color);
+    btn.classList.add("flash");
 
-};
+    setTimeout(() => {
+        btn.classList.remove("flash");
+    }, 110);
+}
+
+function userFlashButton(color) {
+    let btn = document.getElementById(color);
+    btn.classList.add("userFlash");
+
+    setTimeout(() => {
+        btn.classList.remove("userFlash");
+    }, 110);
+}
 
 function wrongAns() {
     document.querySelector("body").style.backgroundColor = "red";
@@ -62,47 +85,22 @@ function wrongAns() {
     }, 150);
 };
 
-function checkAns() {
-    let idx = userSeq.length - 1;  // Compare based on current input index
-
-    if (userSeq[idx] !== gameSeq[idx]) {  
-        displayText.innerHTML = `Game Over!<br> Your Score : <b> **${level}**<b> <br> Press any key to START again`;
+// Check User Input
+function checkAnswer(currentIndex) {
+    if (userSequence[currentIndex] !== gameSequence[currentIndex]) { 
+        levelText.innerHTML = `Game Over!<br> Your Score : <b> **${level}**<b> <br> Press any key to START again`;
         wrongAns();
-        reset();
-        return;  // Exit early to prevent further execution
+        setTimeout(function () {
+            modal.style.display = "block";  // Show modal again
+            levelText.innerText = "1";
+        }, 4000);
+        return;
     }
 
-    // If user sequence matches completely, move to next level
-    if (userSeq.length === gameSeq.length) {
-        setTimeout(levelUp, 1000);
+    if (userSequence.length === gameSequence.length) {
+        setTimeout(() => {
+            level++;
+            nextLevel();
+        }, 800);
     }
-};
-
-
-function btnPressed(event) {
-    let btn = event.target;
-
-    userBtnFlash(btn);
-
-    userColor = btn.getAttribute("id");
-    userSeq.push(userColor);
-    console.log(userSeq);
-
-    checkAns(userSeq.length - 1);
-};
-
-let allBtns = document.querySelectorAll(".btn");
-for (btn of allBtns) {
-    btn.addEventListener("click", btnPressed);
-};
-
-function reset() {
-    started = false;
-    gameSeq = [];
-    userSeq = [];
-    level = 0;
-};
-
-
-
-
+}
